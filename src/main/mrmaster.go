f926e904fbcbce90d9115d5d99c639b2c14e636d
石俊杰 <31870027+intrepiditee@users.noreply.github.com>
2020-08-27 16:30:58 -0500

    add files

---
+++ src/main/mrmaster.go
@@ -0,0 +1,29 @@
+package main
+
+//
+// start the master process, which is implemented
+// in ../mr/master.go
+//
+// go run mrmaster.go pg*.txt
+//
+
+import (
+	"fmt"
+	"mr"
+	"os"
+	"time"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: mrmaster inputfiles...\n")
+		os.Exit(1)
+	}
+
+	m := mr.MakeMaster(os.Args[1:], 10)
+	for m.Done() == false {
+		time.Sleep(time.Second)
+	}
+
+	time.Sleep(time.Second)
+}
